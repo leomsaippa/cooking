57e9e030489d2c9f7826815d2083550aaa7b5f28
leomsaippa <leonardo_saippa@id.uff.br>
2019-01-13 16:51:54 -0200

    Separates layouts from adapter and adds treatment for multiples ViewHolder.

--- app/src/main/java/udacity/lsaippa/cooking/ui/detail/recipe/DetailRecipeAdapter.java
+++ app/src/main/java/udacity/lsaippa/cooking/ui/detail/recipe/DetailRecipeAdapter.java
@@ -11,15 +11,16 @@ import java.util.List;
 
 import udacity.lsaippa.cooking.R;
 import udacity.lsaippa.cooking.network.model.Ingredient;
-import udacity.lsaippa.cooking.network.model.Recipe;
 import udacity.lsaippa.cooking.network.model.Step;
-import udacity.lsaippa.cooking.ui.main.RecipeAdapter;
 
-public class DetailRecipeAdapter extends RecyclerView.Adapter<DetailRecipeAdapter.ItemViewHolder>{
+public class DetailRecipeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
 
     private List<Ingredient> ingredients;
     private List<Step> steps;
+
+    private static final int INGREDIENT = 0;
+    private static final int STEP = 1;
 
     public interface OnStepClickListener{
         void onClickStep(Step step);
@@ -39,49 +40,95 @@ public class DetailRecipeAdapter extends RecyclerView.Adapter<DetailRecipeAdapte
 
     @NonNull
     @Override
-    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public  RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(viewGroup.getContext());
 
-        View view = layoutInflater.inflate(R.layout.detail_recipe_view_holder, viewGroup, false);
-        return new ItemViewHolder(view);
+        View view;
+        RecyclerView.ViewHolder viewHolder;
+        switch (viewType){
+            case INGREDIENT:
+                view = layoutInflater.inflate(R.layout.detail_recipe_ingredients_view_holder,viewGroup,false);
+                viewHolder = new IngredientsViewHolder(view);
+                break;
+            case STEP:
+                view = layoutInflater.inflate(R.layout.detail_recipe_steps_view_holder, viewGroup, false);
+                viewHolder = new StepsViewHolder(view);
+                break;
+            default:
+                view = layoutInflater.inflate(R.layout.detail_recipe_steps_view_holder, viewGroup, false);
+                viewHolder = new StepsViewHolder(view);
+                break;
+
+        }
+
+        return viewHolder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+        int viewType = getItemViewType(i);
+
+        switch (viewType){
+
+            case INGREDIENT:
+                Ingredient ingredient = ingredients.get(i);
+                IngredientsViewHolder ingredientViewHolder = (IngredientsViewHolder) viewHolder;
+
+                ingredientViewHolder.mIngredient.setText(ingredient.getIngredient());
+
+                String quantityAndMeasure = String.format(ingredientViewHolder.itemView.getContext().getString(R.string.ingredient_quantity_and_measure),
+                        ingredient.getQuantity(), ingredient.getMeasure());
+
+                ingredientViewHolder.mQuantity.setText(quantityAndMeasure);
+
+                break;
+            case STEP:
+                Step step = steps.get(getStepPosition(i));
+                StepsViewHolder stepsViewHolder = (StepsViewHolder) viewHolder;
+                String stepNumberText = getStepPosition(i) + 1 + " )" + step.getShortDescription();
+                stepsViewHolder.mStepName.setText(stepNumberText);
+
+                break;
+        }
 
 
 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ItemViewHolder itemViewHolder, int i) {
-
-        Ingredient ingredient = ingredients.get(i);
-        Step step = steps.get(i);
-        itemViewHolder.mIngredient.setText(ingredient.getIngredient());
-
-        String quantityAndMeasure = String.format(itemViewHolder.itemView.getContext().getString(R.string.ingredient_quantity_and_measure),
-                ingredient.getQuantity(), ingredient.getMeasure());
-
-        itemViewHolder.mQuantity.setText(quantityAndMeasure);
-        String stepNumberText = i  + 1 + " )" + step.getShortDescription();
-        itemViewHolder.mStepName.setText(stepNumberText);
-
-
+    public int getItemViewType(int position) {
+        if(position<ingredients.size()){
+            return INGREDIENT;
+        }else{
+            return STEP;
+        }
     }
+
 
     @Override
     public int getItemCount() {
-        return 5;
+        int total = 0;
+        if(steps!=null){
+            total = steps.size();
+        }
+        if(ingredients!=null){
+            total = total + ingredients.size();
+        }
+        return total;
     }
 
+    public int getStepPosition(int adapterPosition){
+        return adapterPosition - ingredients.size();
+    }
 
-    public class ItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public class StepsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private final TextView mIngredient;
-        private final TextView mQuantity;
         private final TextView mStepName;
 
-        ItemViewHolder(@NonNull View view) {
+        StepsViewHolder(@NonNull View view) {
             super(view);
-            mIngredient = view.findViewById(R.id.tv_ingredient_name);
-            mQuantity = view.findViewById(R.id.tv_ingredient_qnt);
             mStepName = view.findViewById(R.id.tv_step_name);
             view.setOnClickListener(this);
 
@@ -89,7 +136,26 @@ public class DetailRecipeAdapter extends RecyclerView.Adapter<DetailRecipeAdapte
 
         @Override
         public void onClick(View v) {
-            Step step = steps.get(getAdapterPosition());
+            Step step = steps.get(getStepPosition(getAdapterPosition()));
             onStepClickListener.onClickStep(step);        }
+    }
+    public class IngredientsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private final TextView mIngredient;
+        private final TextView mQuantity;
+
+        IngredientsViewHolder(@NonNull View view) {
+            super(view);
+            mIngredient = view.findViewById(R.id.tv_ingredient_name);
+            mQuantity = view.findViewById(R.id.tv_ingredient_qnt);
+            view.setOnClickListener(this);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+            Step step = steps.get(getStepPosition(getAdapterPosition()));
+            onStepClickListener.onClickStep(step);
+        }
     }
 }
