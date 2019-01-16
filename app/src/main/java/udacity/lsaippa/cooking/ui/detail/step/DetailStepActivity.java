c15c9652730e13460e69fe608e3dae92b9e16044
Leonardo Saippa <lsaippa@muxi.com.br>
2019-01-16 08:50:44 -0200

    Adds transaction between fragments based on step selected.

--- app/src/main/java/udacity/lsaippa/cooking/ui/detail/step/DetailStepActivity.java
+++ app/src/main/java/udacity/lsaippa/cooking/ui/detail/step/DetailStepActivity.java
@@ -36,6 +36,30 @@ public class DetailStepActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_step);
+
+        setup();
+
+        mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                currentStep = steps.get(tab.getPosition());
+                openNewStepTab(currentStep);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+    private void setup() {
         ButterKnife.bind(this);
         mTabLayout.setupWithViewPager(mViewPager);
 
@@ -49,23 +73,35 @@ public class DetailStepActivity extends AppCompatActivity {
         }
 
         for(int i=0;i<quantitySteps;i++){
-            int tabShowing = i +1 ;
-            mTabLayout.addTab(mTabLayout.newTab().setText(String.valueOf(tabShowing)));
+            String tabShowing = String.valueOf(i);
+            if(tabShowing.equals("0"))
+                tabShowing = "Intro";
+            mTabLayout.addTab(mTabLayout.newTab().setText(tabShowing));
         }
 
 
-        FragmentManager fragMan = getSupportFragmentManager();
-        FragmentTransaction fragTransaction = fragMan.beginTransaction();
+        //Todo move screen to correct position view
+        TabLayout.Tab tab = mTabLayout.getTabAt(currentStepId);
+
+        if(tab!=null)
+            tab.select();
+
+        openNewStepTab(currentStep);
+    }
+
+    private void openNewStepTab(Step currentStep) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragTransaction = fragmentManager.beginTransaction();
 
         Bundle newBundle = new Bundle();
         newBundle.putParcelable(STEP_TAG, currentStep);
         Fragment fragment = new DetailStepFragment();
         fragment.setArguments(newBundle);
 
-        fragTransaction.add(R.id.step_container, fragment , DetailStepActivity.class.getSimpleName()) ;
+        fragTransaction.replace(R.id.step_container, fragment);
+        fragmentManager.popBackStack();
 
         fragTransaction.commit();
-
     }
 
     private void setToolbarNameRecipe(String recipeName) {
